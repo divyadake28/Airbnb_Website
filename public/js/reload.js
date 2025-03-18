07022418610f500fcea33f6cba4063e3bb65ca01
divyadake28 <divyadake25@gmail.com>
2025-03-18 18:12:59 +0530

    Add Project Files

---
+++ public/js/reload.js
@@ -0,0 +1,51 @@
+let filter = document.querySelectorAll(".filter")
+
+filter[0].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[1].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[2].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[3].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[4].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[5].addEventListener("click", () =>{
+    location.reload();
+});
+
+
+filter[6].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[7].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[8].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[9].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[10].addEventListener("click", () =>{
+    location.reload();
+});
+
+filter[11].addEventListener("click", () =>{
+    location.reload();
+});
+
